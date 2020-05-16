7c88d9c469c0a6ed9d0023405b18ee28792d0da0
cadumancini <cadumancini@gmail.com>
2020-05-16 15:24:25 -0300

    User can change all 8 possible values of the border-radius in order to create a complex shape

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+var advanced = false;
+
+const topLeft = document.getElementById('input_tl');
+const topRight = document.getElementById('input_tr');
+const bottomRight = document.getElementById('input_br');
+const bottomLeft = document.getElementById('input_bl');
+
+const box = document.getElementById('box-div');
+
+const adv_tl_rx = document.getElementById("adv-tl-rx");
+const adv_tr_rx = document.getElementById("adv-tr-rx");
+const adv_br_rx = document.getElementById("adv-br-rx");
+const adv_bl_rx = document.getElementById("adv-bl-rx");
+const adv_tl_ry = document.getElementById("adv-tl-ry");
+const adv_tr_ry = document.getElementById("adv-tr-ry");
+const adv_br_ry = document.getElementById("adv-br-ry");
+const adv_bl_ry = document.getElementById("adv-bl-ry");
+
 function changeBorderRadius() {
-  const topLeft = document.getElementById('input_tl');
-  const topRight = document.getElementById('input_tr');
-  const bottomRight = document.getElementById('input_br');
-  const bottomLeft = document.getElementById('input_bl');
   
-  const box = document.getElementById('box-div');
+  box.style.borderRadius = "";
   box.style.borderTopLeftRadius = topLeft.value + "px";
   box.style.borderTopRightRadius = topRight.value + "px";
   box.style.borderBottomRightRadius = bottomRight.value + "px";
@@ -12,16 +26,24 @@ function changeBorderRadius() {
 }
 
 function copyToClipboard() {
-  const topLeft = document.getElementById('input_tl');
-  const topRight = document.getElementById('input_tr');
-  const bottomRight = document.getElementById('input_br');
-  const bottomLeft = document.getElementById('input_bl');
   
   var textArea = document.createElement("textarea");
-  textArea.value = "border-top-left-radius: " + topLeft.value + ";\n";
-  textArea.value += "border-top-right-radius: " + topRight.value + ";\n";
-  textArea.value += "border-bottom-right-radius: " + bottomRight.value + ";\n";
-  textArea.value += "border-bottom-left-radius: " + bottomLeft.value + ";";
+
+  if(advanced) {
+    textArea.value = "border-radius: " + adv_tl_rx.value + "% " + 
+                              adv_tr_rx.value + "% " + 
+                              adv_br_rx.value + "% " + 
+                              adv_bl_rx.value + "% / " + 
+                              adv_tl_ry.value + "% " + 
+                              adv_tr_ry.value + "% " + 
+                              adv_br_ry.value + "% " + 
+                              adv_bl_ry.value + "%";
+  } else {
+    textArea.value = "border-top-left-radius: " + topLeft.value + ";\n";
+    textArea.value += "border-top-right-radius: " + topRight.value + ";\n";
+    textArea.value += "border-bottom-right-radius: " + bottomRight.value + ";\n";
+    textArea.value += "border-bottom-left-radius: " + bottomLeft.value + ";";
+  }
   
   // Avoid scrolling to bottom
   textArea.style.top = "0";
@@ -35,4 +57,43 @@ function copyToClipboard() {
   document.execCommand("copy");
   document.body.removeChild(textArea);
   alert("Properties successfully copied to clipboard!");
+}
+
+function toggleAdvanced() {
+  advanced = !advanced;
+  enableInputs(advanced);
+
+  if(advanced) {
+    document.getElementById("footer").style.visibility = "visible";
+    changeBorderAdvanced();
+  }
+  else {
+    document.getElementById("footer").style.visibility = "hidden";
+    changeBorderRadius();
+  }
+}
+
+function enableInputs(enable) {
+  topLeft.disabled = enable;
+  topRight.disabled = enable;
+  bottomRight.disabled = enable;
+  bottomLeft.disabled = enable;
+}
+
+function changeBorderAdvanced() {
+  box.style.borderTopLeftRadius = "";
+  box.style.borderTopRightRadius = "";
+  box.style.borderBottomRightRadius = "";
+  box.style.borderBottomLeftRadius = "";
+
+  const border = adv_tl_rx.value + "% " + 
+                  adv_tr_rx.value + "% " + 
+                  adv_br_rx.value + "% " + 
+                  adv_bl_rx.value + "% / " + 
+                  adv_tl_ry.value + "% " + 
+                  adv_tr_ry.value + "% " + 
+                  adv_br_ry.value + "% " + 
+                  adv_bl_ry.value + "%";
+
+  box.style.borderRadius = border;
 }
